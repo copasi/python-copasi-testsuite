bc1ad6b45f5640d46fd9b7db657832a3c1913dbe
Frank Bergmann <fbergman@caltech.edu>
2018-08-22 10:49:08 +0200

    - allow to filter tests by optional task name

--- test_copasi.py
+++ test_copasi.py
@@ -17,14 +17,22 @@ if __name__ == "__main__":
     setup_logging()
     
     num_args = len(sys.argv)
-    if num_args != 4:
-        print("usage: test_copasi <Test> <CopasiSE> <OutputDir>")
+    if num_args < 4:
+        print("usage: test_copasi <Test> <CopasiSE> <OutputDir> [Task Name]")
         sys.exit(1)
 
     suite = TestSuite(sys.argv[1])
     runner = TestRunner(sys.argv[2], os.path.abspath(sys.argv[3]))
-    
-    result = runner.runTests(suite.cases)
+
+    task_name = None
+    if num_args == 5:
+        task_name = sys.argv[4]
+
+    cases = suite.cases
+    if task_name is not None:
+        cases = [c for c in cases if c.task_type == task_name]
+
+    result = runner.runTests(cases)
     
     if result == 0: 
         print("\nall pass")
